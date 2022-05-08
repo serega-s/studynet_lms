470d947593c9e6af863ad6cf485c4d14f8dde286
serega-s <comp.it.05@inbox.ru>
2022-05-08 18:04:55 +0300

    New model Activity

---
+++ studynet_django/activity/models.py
@@ -0,0 +1,28 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+from course.models import Lesson, Course
+
+User = get_user_model()
+
+
+class Activity(models.Model):
+    STARTED = 'started'
+    DONE = 'done'
+
+    STATUS_CHOICES = [
+        (STARTED, 'Started'),
+        (DONE, 'Done')
+    ]
+
+    course = models.ForeignKey(Course, related_name='activities', on_delete=models.CASCADE)
+    lesson = models.DateTimeField(Lesson, related_name='activities', on_delete=models.CASCADE)
+    status = models.CharField(max_length=25, choices=STATUS_CHOICES, default=STARTED)
+    created_by = models.DateTimeField(User, related_name='activities', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return {self.course.title}
+
+    class Meta:
+        verbose_name_plural = 'Activities'
