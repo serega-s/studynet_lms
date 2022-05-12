a4ad8eebe3175051d0517669050cdaac3fa58fd8
serega-s <comp.it.05@inbox.ru>
2022-05-12 19:57:47 +0300

    Schemas generation with OpenAPI

--- studynet_django/studynet_django/urls.py
+++ studynet_django/studynet_django/urls.py
@@ -2,6 +2,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework.permissions import AllowAny
+from rest_framework.schemas import get_schema_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,6 +13,15 @@ urlpatterns = [
     path('api/v1/activities/', include('activity.urls')),
 
     path('__debug__/', include('debug_toolbar.urls')),
+
+    path(
+        'openapi/', get_schema_view(
+            title="StudyNet LMS",
+            description="API for our LMS",
+            version="1.0.0",
+            permission_classes=[AllowAny],
+        ), name='openapi-schema'
+    ),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
