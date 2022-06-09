ef2e7fbc18d2163a328d77c8fdeb1b930c02ed34
serega-s <comp.it.05@inbox.ru>
2022-06-09 09:45:22 +0300

    added view for filtering author courses

--- studynet_django/course/urls.py
+++ studynet_django/course/urls.py
@@ -7,10 +7,16 @@ from . import views
 urlpatterns = [
     path('', views.CourseListAPIView.as_view()),
     path('get-categories/', views.CategoryListAPIView.as_view()),
+    path('get-author-courses/<int:user_id>/',
+         views.AuthorCourseListAPIView.as_view()),
     path('<slug:slug>/', views.GetCourseApiView.as_view()),
-    path('<slug:course_slug>/<slug:lesson_slug>/get-quiz/', views.GetQuizAPIView.as_view()),
-    path('<slug:course_slug>/<slug:lesson_slug>/', views.AddCommentAPIView.as_view()),
-    path('<slug:course_slug>/<slug:lesson_slug>/get-comments/', views.GetCommentsAPIView.as_view()),
+    path('<slug:course_slug>/<slug:lesson_slug>/get-quiz/',
+         views.GetQuizAPIView.as_view()),
+    path('<slug:course_slug>/<slug:lesson_slug>/',
+         views.AddCommentAPIView.as_view()),
+    path('<slug:course_slug>/<slug:lesson_slug>/get-comments/',
+         views.GetCommentsAPIView.as_view()),
+
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- studynet_django/course/views.py
+++ studynet_django/course/views.py
@@ -1,13 +1,13 @@
-from rest_framework import permissions, generics
+from django.contrib.auth.models import User
+from rest_framework import generics, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from course.models import Category, Comment, Course, Lesson
-from course.serializers import (
-    CategorySerializer, CommentSerializer,
-    CourseDetailSerializer, CourseListSerializer,
-    LessonListSerializer, QuizSerializer
-)
+from course.serializers import (CategorySerializer, CommentSerializer,
+                                CourseDetailSerializer, CourseListSerializer,
+                                LessonListSerializer, QuizSerializer,
+                                UserSerializer)
 
 
 class GetQuizAPIView(APIView):
@@ -32,13 +32,10 @@ class GetCourseApiView(APIView):
     def get(self, request, slug):
         course = Course.objects.get(slug=slug)
         course_serializer = CourseDetailSerializer(course)
-        lesson_serializer = LessonListSerializer(course.lessons.all(), many=True)
+        lesson_serializer = LessonListSerializer(
+            course.lessons.all(), many=True)
 
-        if request.user.is_authenticated:
-            course_data = course_serializer.data
-        else:
-            course_data = {}
-
+        course_data = course_serializer.data if request.user.is_authenticated else {}
         return Response(
             {
                 'course': course_data,
@@ -47,20 +44,35 @@ class GetCourseApiView(APIView):
         )
 
 
+class AuthorCourseListAPIView(APIView):
+    def get(self, request, user_id):
+        courses = Course.objects.prefetch_related(
+            'categories').select_related('created_by').filter(created_by=user_id).order_by('-created_at')
+        user = User.objects.get(pk=user_id)
+        courses_serializer = CourseListSerializer(courses, many=True)
+        user_serializer = UserSerializer(user)
+
+        return Response(
+            {
+                'courses': courses_serializer.data,
+                'created_by': user_serializer.data
+            }
+        )
+
+
 class CourseListAPIView(generics.ListAPIView):
-    queryset = Course.objects.all().prefetch_related('categories').order_by('-created_at')
+    queryset = Course.objects.prefetch_related(
+        'categories').select_related('created_by').all().order_by('-created_at')
     permission_classes = [permissions.AllowAny]
     authentication_classes = []
     serializer_class = CourseListSerializer
 
     def get_queryset(self):
-        category_id = self.request.query_params.get('category_id')
         queryset = super().get_queryset()
-        if category_id:
+        if category_id := self.request.query_params.get('category_id'):
             queryset = queryset.filter(categories__in=[int(category_id)])
 
-        limit = self.request.query_params.get('limit')
-        if limit:
+        if limit := self.request.query_params.get('limit'):
             queryset = queryset[:int(limit)]
 
         return queryset
@@ -68,7 +80,7 @@ class CourseListAPIView(generics.ListAPIView):
 
 class GetCommentsAPIView(APIView):
     def get(self, request, course_slug, lesson_slug):
-        lesson = Lesson.objects.get(slug=lesson_slug)
+        lesson = Lesson.objects.select_related('course').get(slug=lesson_slug)
         serializer = CommentSerializer(lesson.comments.all(), many=True)
         return Response(serializer.data)
 
