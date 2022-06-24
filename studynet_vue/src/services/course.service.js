51f95ac1085757c543a8367a5b85d573a6985d96
serega-s <comp.it.05@inbox.ru>
2022-06-24 13:09:17 +0300

    added new service in frontend

--- studynet_vue/src/services/course.service.js
+++ studynet_vue/src/services/course.service.js
@@ -5,27 +5,28 @@ class CourseService {
     return await axios.get("courses/get-categories/")
   }
 
+  async createCourse(data) {
+    return await axios.post("courses/create/", data)
+  }
+
   async getCourses(url) {
     return await axios.get(url)
   }
 
   async getComments(courseSlug, activeLessonSlug) {
     return await axios.get(
-        `courses/${courseSlug}/${activeLessonSlug}/get-comments/`
+      `courses/${courseSlug}/${activeLessonSlug}/get-comments/`
     )
   }
 
   async getQuiz(courseSlug, activeLessonSlug) {
     return await axios.get(
-        `courses/${courseSlug}/${activeLessonSlug}/get-quiz/`
+      `courses/${courseSlug}/${activeLessonSlug}/get-quiz/`
     )
   }
 
   async submitComment(courseSlug, activeLessonSlug, data) {
-    return await axios.post(
-        `courses/${courseSlug}/${activeLessonSlug}/`,
-        data
-    )
+    return await axios.post(`courses/${courseSlug}/${activeLessonSlug}/`, data)
   }
 
   async getCoursesLessonsData(slug) {
