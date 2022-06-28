09c0a8fc1b2300771613173a3abc099b8a593e01
serega-s <comp.it.05@inbox.ru>
2022-06-28 22:29:50 +0300

    Possibility to add lessons to course from frontend

--- studynet_vue/src/views/dasboard/CreateCourse.vue
+++ studynet_vue/src/views/dasboard/CreateCourse.vue
@@ -7,8 +7,9 @@
     </div>
     <section class="section">
       <div class="columns is-centered">
-        <div class="column is-6">
-          <form @submit.prevent="submitForm">
+        <div class="column is-8">
+          <div class="mb-6 px-6 py-4 has-background-grey-lighter">
+            <h2 class="subtitle">Meta information</h2>
             <div class="field">
               <label for="title">Title</label>
               <input type="text" class="input" v-model="form.title" />
@@ -39,10 +40,59 @@
                 </select>
               </div>
             </div>
-            <div class="field">
-              <button class="button is-success">Submit</button>
+          </div>
+          <div class="mb-6 px-6 py-4 has-background-grey-lighter">
+            <h2 class="subtitle">Lessons</h2>
+
+            <button class="button is-primary" @click="addLesson">
+              Add lesson
+            </button>
+
+            <div
+              v-for="(lesson, index) in form.lessons"
+              :key="index"
+              class="mb-4"
+            >
+              <h3 class="subtitle is-size-6">Lesson</h3>
+
+              <div class="field">
+                <label for="title">Title</label>
+                <input type="text" class="input" v-model="lesson.title" />
+                <!-- :name="form.lessons[index][title]" -->
+              </div>
+
+              <div class="field">
+                <label for="short_description">Short description</label>
+                <textarea
+                  type="text"
+                  class="input"
+                  v-model="lesson.short_description"
+                />
+                <!-- :name="form.lessons[index][short_description]" -->
+              </div>
+
+              <div class="field">
+                <label for="long_description">Long description</label>
+                <textarea
+                  type="text"
+                  class="textarea"
+                  v-model="lesson.long_description"
+                />
+                <!-- :name="form.lessons[index][long_description]" -->
+              </div>
             </div>
-          </form>
+
+            <hr />
+          </div>
+
+          <div class="field buttons">
+            <button class="button is-success m-1" @click="submitForm('draft')">
+              Save as draft
+            </button>
+            <button class="button is-info m-1" @click="submitForm('in_review')">
+              Submit for review
+            </button>
+          </div>
         </div>
       </div>
     </section>
@@ -60,6 +110,14 @@ export default {
         short_description: "",
         long_description: "",
         categories: [],
+        status: "",
+        lessons: [
+          {
+            title: "",
+            short_description: "",
+            long_description: "",
+          },
+        ],
       },
 
       categories: [],
@@ -79,8 +137,9 @@ export default {
       }
       this.$store.commit("setIsLoading", false)
     },
-    async submitForm() {
+    async submitForm(status) {
       this.$store.commit("setIsLoading", true)
+      this.form.status = status
       try {
         const response = await CourseService.createCourse(this.form)
         console.log(response.data)
@@ -88,6 +147,14 @@ export default {
         console.error(e.response)
       }
       this.$store.commit("setIsLoading", false)
+    },
+
+    addLesson() {
+      this.form.lessons.push({
+        title: "",
+        short_description: "",
+        long_description: "",
+      })
     },
   },
 }
